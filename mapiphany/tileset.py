29935eb67d688b12a95f127923aa135cc4c93bef
Cory Dodt <corymercurial@spam.goonmill.org>
2011-01-26 11:09:49 -0800

    tileset.py writes out a merged tileset for each tileset.ini directory

--- mapiphany/tileset.py
+++ mapiphany/tileset.py
@@ -1,10 +1,23 @@
 """
 Manage tilesets and load the default tileset
 """
+import os
 import sys
 from ConfigParser import ConfigParser
 
 from twisted.python.filepath import FilePath
+
+import simplejson
+
+
+def discoverTilesets(path):
+    """
+    Walk path and look for directories containing tileset.ini; return tilesets
+    for them.
+    """
+    for fp in path.walk():
+        if fp.child('tileset.ini').exists():
+            yield TileSet(fp.path)
 
 
 class TileSet(FilePath):
@@ -17,67 +30,90 @@ class TileSet(FilePath):
         self.ini.read(self.child('tileset.ini').path)
         self.name = self.path.split('/')[-1]
 
-        # the default tileset comes from the parent directory and is built by
-        # merging all the children
-        if self.ini.has_section('default'):
-            self.mergeChildren()
-        else:
-            for sect in self.ini.sections():
-                self.ini.set(sect, 'set', self.name)
+        for sect in self.ini.sections():
+            self.ini.set(sect, 'set', self.name)
 
-    def mergeChildren(self):
+    @classmethod
+    def applyMerge(cls, tileset):
         """
-        Find all child tilesets and merge their configuration into self
+        Apply section [__default__]'s order attribute, importing tiles from
+        other tilesets and merging them.  This returns a new tileset 
         """
-        ini = self.ini
-        order = ini.get('default', 'tilesetOrder').strip().split('\n')
-        for _ts in reversed(order):
-            ts = TileSet(self.child(_ts).path)
+        new1 = TileSet(tileset.path)
+        for ts in reversed(tileset.orderSets()):
             for sect in ts.ini.sections():
-                if not ini.has_section(sect):
-                    ini.add_section(sect)
+                if not new1.ini.has_section(sect):
+                    new1.ini.add_section(sect)
 
                 # prevent overwriting a non-blank icon with blank
-                if ini.has_option(sect, 'iconfilename') and not ts.ini.get(sect, 'iconfilename'):
+                if tileset.ini.has_option(sect, 'iconfilename') and not ts.ini.get(sect, 'iconfilename'):
                     continue
 
                 for k, v in ts.ini.items(sect):
-                    ini.set(sect, k, v)
+                    new1.ini.set(sect, k, v)
+
+        return new1
 
     def __iter__(self):
         for sect in self.ini.sections():
-            if sect == 'default':
+            if sect == '__default__':
                 continue
             d = dict(self.ini.items(sect))
             yield sect, d
 
+    def orderSets(self):
+        """
+        Return the childsets mentioned in [__default__].order section of my
+        tileset.ini
+        """
+        ret = []
+        ini = self.ini
+        order = ini.get('__default__', 'order').strip().split(':')
+        for _ts in order:
+            if _ts == '.':
+                ret.append(self)
+            else:
+                absPath = os.path.abspath(self.path + '/' + _ts)
+                ret.append(TileSet(absPath))
 
-defaultSet = TileSet('tiles')
+        return ret
+
+    def writeResources(self):
+        """
+        Generate required .js and .css files for this tileset
+        """
+        merged = TileSet.applyMerge(self)
+
+        jsFile = merged.child('tileset.js').open('w')
+        jsFile.write('var gTileset = ')
+
+        cssFile = merged.child('tileset.css').open('w')
+
+        tileset = {}
+        categories = {}
+        for name, tile in merged:
+            if name == '__default__':
+                continue
+
+            tileset[name] = tile
+            categories.setdefault(tile['category'], []).append(name)
+            color = tile['backgroundrgb'].lower();
+            cssFile.write('.%s { fill: %s; background-color: %s; }\n' % (
+                name, color, color))
+        simplejson.dump(tileset, jsFile, sort_keys=True, indent=4 * ' ')
+        jsFile.write(';\nvar gTileCategories = ')
+        categories = dict(map(lambda x: (x[0], sorted(x[1])), categories.items()))
+        simplejson.dump(categories, jsFile, sort_keys=True, indent=4 * ' ')
+        jsFile.write(';\n')
+
 
 
 def run(argv=None):
     if argv is None:
         argv = sys.argv
 
-    import simplejson
-    jsFile = open('tileset.js', 'w')
-    jsFile.write('var gTileset = ')
-
-    cssFile = open('tileset.css', 'w')
-
-    tileset = {}
-    categories = {}
-    for name, tile in defaultSet:
-        tileset[name] = tile
-        categories.setdefault(tile['category'], []).append(name)
-        color = tile['backgroundrgb'].lower();
-        cssFile.write('.%s { fill: %s; background-color: %s; }\n' % (
-            name, color, color))
-    simplejson.dump(tileset, jsFile, sort_keys=True, indent=4 * ' ')
-    jsFile.write(';\nvar gTileCategories = ')
-    categories = dict(map(lambda x: (x[0], sorted(x[1])), categories.items()))
-    simplejson.dump(categories, jsFile, sort_keys=True, indent=4 * ' ')
-    jsFile.write(';\n')
+    for ts in discoverTilesets(FilePath('tiles')):
+        ts.writeResources()
 
 
 if __name__ == '__main__':
